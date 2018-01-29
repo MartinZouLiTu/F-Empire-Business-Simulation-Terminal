42d5891e29cc48d07ecff8185497914d81249318
Martin Zou <martyzou20010126@outlook.com>
2018-01-29 18:52:48 +0800

    Added database configuration

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+$server_address='localhost:3306';
+$sql_user='admin';
+$sql_password='picontrol';
+$db_name='company';
+$db=mysqli_connect($server_address,$sql_user,$sql_password,$db_name) or die('Error connecting to server');
+?>
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-print("it works!");
+phpinfo();
 ?>
