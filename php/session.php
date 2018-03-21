2e84800b683f6dd78a02c607d4737c18b21c3222
Marty <martyzou20010126@outlook.com>
2018-03-21 21:49:28 +0800

    unified global var, all global vars can be found in session.php

--- php/session.php
+++ php/session.php
@@ -7,7 +7,7 @@ else{
     $currentID=$_SESSION['login_user'];
     include('db.php');
     $currentYear = mysqli_fetch_array(mysqli_query($db,'select year_now() as year'))['year'];
-    $query = 'SELECT username,company_name,cash,loan,decision_allowed,total_fac(???,0) as perm_fac, total_fac(???,!!!), year_now() as year,news_now() as news FROM company_info WHERE user_id=???';
+    $query = 'SELECT username,company_name,cash,loan,decision_allowed,total_fac(???,0) as perm_fac, total_fac(???,!!!) as total_fac, year_now() as year,news_now() as news FROM company_info WHERE user_id=???';
     $query = str_replace('???',$currentID,$query);
     $query = str_replace('!!!',$currentYear,$query);
     $user_profile=mysqli_fetch_array(mysqli_query($db,$query),MYSQLI_ASSOC);
