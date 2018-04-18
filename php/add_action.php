1268b7dc2dd8b0fb642d68d39e77d120a6bfa66d
Marty <martyzou20010126@outlook.com>
2018-04-18 21:34:28 +0800

    Added isSave

--- php/add_action.php
+++ php/add_action.php
@@ -8,14 +8,26 @@ if(!isset($_SESSION['login_user'])){
 }
 else{
     $input = json_decode(file_get_contents("php://input"),true);
-    $insert_sql='insert into action (uid, yid, pid, price, add_ads, service_level, pkg, qty) VALUES (?,year_now(),?,?,?,?,?,?)';
-    $insert_stmt=$db->prepare($insert_sql);
-    $insert_stmt->bind_param('iiiiiii',$_SESSION['login_user'],$input['pid'],$input['sellcode'],$input['adverts'],$input['selService'],$input['packaging'],$input['qty']);
-    if($insert_stmt->execute()){
-        print true;
+    $check_sql='select check_save(?,?) as isSave';
+    $check_stmt=$db->prepare($check_sql);
+    $check_stmt->bind_param('ii',$_SESSION['login_user'],$input['pid']);
+    $check_stmt->bind_result($isSave);
+    $check_stmt->execute();
+    $check_stmt->fetch();
+    if($isSave==0){
+        $check_stmt->close();
+        $insert_sql='insert into action (uid, yid, pid, price, add_ads, service_level, pkg, qty) VALUES (?,year_now(),?,?,?,?,?,?)';
+        $insert_stmt=$db->prepare($insert_sql);
+        $insert_stmt->bind_param('iiiiiii',$_SESSION['login_user'],$input['pid'],$input['sellcode'],$input['adverts'],$input['selService'],$input['packaging'],$input['qty']);
+        if($insert_stmt->execute()){
+            print (1);
+        }
+        else{
+            print (0);
+        }
     }
     else{
-        print false;
+        print (0);
     }
 
 }
