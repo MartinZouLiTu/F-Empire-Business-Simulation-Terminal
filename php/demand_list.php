533839e5d5db342b741ceeee598a3fcae775161a
Marty <MartyZou20010126@outlook.com>
2018-09-12 20:32:36 +0800

    added alter global var

--- php/demand_list.php
+++ php/demand_list.php
@@ -1,6 +1,6 @@
 <?php
 include('db.php');
-$demand_sql = 'select * from demand where pid=year_now()';
+$demand_sql = 'select * from demand where yid=year_now()';
 $demand_stmt = $db ->prepare($demand_sql);
 $demand_stmt->execute();
 $result = $demand_stmt->get_result();
