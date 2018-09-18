ea599e8ca4422d041e7d5dff743d33a92b19a860
Marty <MartyZou20010126@outlook.com>
2018-09-18 20:18:44 +0800

    pushed deduction for gv

---
+++ php/demand_submit.php
@@ -0,0 +1,28 @@
+<?php
+include('db.php');
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+session_start();
+if(!isset($_SESSION['login_user'])){
+    print('no_session');
+}
+else {
+
+    $output = array();
+    $input = json_decode(file_get_contents("php://input"), true);
+
+    $occu_sql = 'update demand set occu=1 where id=?';
+    $occu_stmt = $db -> prepare($occu_sql);
+    $occu_stmt->bind_param('i',$input['id']);
+    $occu_stmt->execute();
+    $occu_stmt->close();
+
+    $insert_sql='insert into action (uid, yid, pid, price, add_ads, service_level, pkg, qty) VALUES (?,year_now(),?,0,0,0,0,?)';
+    $insert_stmt=$db->prepare($insert_sql);
+    $insert_stmt->bind_param('iii',$_SESSION['login_user'],$input['pid'],$input['qty']);
+    $insert_stmt->execute();
+    $insert_stmt->close();
+
+
+
+}
