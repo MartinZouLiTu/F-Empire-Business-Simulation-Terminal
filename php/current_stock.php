78ef4e6775152371e0b400f0c0b165daf9713c89
Marty <MartyZou20010126@outlook.com>
2018-09-20 22:48:17 +0800

    New update

---
+++ php/current_stock.php
@@ -0,0 +1,19 @@
+<?php
+include('db.php');
+$output=array();
+session_start();
+$currentID=$_SESSION['login_user'];
+for($i=1;$i<=5;$i++){
+    $request_sql = "select sum(qty) from `action` where uid=? and pid=?";
+    $request_stmt = $db ->prepare($request_sql);
+    $request_stmt->bind_param('ii',$currentID,$i);
+    $request_stmt->execute();
+    $request_stmt->bind_result($result);
+    $request_stmt->fetch();
+    $result=$result==null ? 0 : $result;
+    array_push($output,(int)$result);
+    $request_stmt->close();
+}
+
+
+print(json_encode($output));
