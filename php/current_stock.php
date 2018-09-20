a94c406c1aa488e4950e15125bbfc52d18c0dd64
Marty <MartyZou20010126@outlook.com>
2018-09-20 22:59:24 +0800

    New update

--- php/current_stock.php
+++ php/current_stock.php
@@ -3,6 +3,15 @@ include('db.php');
 $output=array();
 session_start();
 $currentID=$_SESSION['login_user'];
+$request_sql = "select sum(qty) from `action` where uid=?";
+$request_stmt = $db ->prepare($request_sql);
+$request_stmt->bind_param('i',$currentID);
+$request_stmt->execute();
+$request_stmt->bind_result($result);
+$request_stmt->fetch();
+$result=$result==null ? 0 : $result;
+array_push($output,(int)$result);
+$request_stmt->close();
 for($i=1;$i<=5;$i++){
     $request_sql = "select sum(qty) from `action` where uid=? and pid=?";
     $request_stmt = $db ->prepare($request_sql);
@@ -14,14 +23,6 @@ for($i=1;$i<=5;$i++){
     array_push($output,(int)$result);
     $request_stmt->close();
 }
-$request_sql = "select sum(qty) from `action` where uid=?";
-$request_stmt = $db ->prepare($request_sql);
-$request_stmt->bind_param('i',$currentID);
-$request_stmt->execute();
-$request_stmt->bind_result($result);
-$request_stmt->fetch();
-$result=$result==null ? 0 : $result;
-array_push($output,(int)$result);
-$request_stmt->close();
+
 
 print(json_encode($output));
