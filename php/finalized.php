ead113c2a75e6bb5c260ec3417a08633449c7fb8
Marty <MartyZou20010126@outlook.com>
2018-09-21 22:46:44 +0800

    New update

--- php/finalized.php
+++ php/finalized.php
@@ -12,4 +12,10 @@ else {
     $final_stmt->bind_param('i',$_SESSION['login_user']);
     $final_stmt->execute();
     $final_stmt->close();
+
+    $log_sql='insert into log (uid, yid, company_value) values (?,year_now(), (select (cash+20) from company_info where user_id=?))';
+    $log_stmt=$db->prepare($log_sql);
+    $log_stmt->bind_param('ii',$_SESSION['login_user'],$_SESSION['login_user']);
+    $log_stmt->execute();
+    $log_sql->close();
 }
