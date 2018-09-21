39d5a8e7636da67d431c5953837b133e6d7bed6e
Marty <MartyZou20010126@outlook.com>
2018-09-21 10:30:42 +0800

    New update

--- php/demand_submit.php
+++ php/demand_submit.php
@@ -16,7 +16,7 @@ else {
     $occu_stmt->bind_param('i',$input['id']);
     $occu_stmt->execute();
     $occu_stmt->close();
-    $output[0]=1;
+
 
 
     $insert_sql='insert into action (uid, yid, pid, price, add_ads, service_level, pkg, qty) VALUES (?,year_now(),?,-?,0,0,0,-?)';
@@ -24,21 +24,22 @@ else {
     $insert_stmt->bind_param('iiii',$_SESSION['login_user'],$input['pid'],$input['id'],$input['qty']);
     $insert_stmt->execute();
     $insert_stmt->close();
-    $output[1]=1;
 
-    $alter_sql='update company_info set cash=cash+current_price(?,?)*?*0.95-loan*0.1, decision_allowed=0 where user_id=? ';
+    $alter_sql='update company_info set cash=cash+current_price(?,?)*?*0.95, decision_allowed=0 where user_id=? ';
     $alter_stmt=$db->prepare($alter_sql);
     $alter_stmt->bind_param('iiii',$_SESSION['login_user'],$input['pid'],$input['qty'],$_SESSION['login_user']);
     $alter_stmt->execute();
     $alter_stmt->close();
-    $output[2]=1;
 
-    $log_sql='insert into log (uid, yid, company_value) values (?,year_now(), (select (cash+20) from company_info where user_id=?))';
-    $log_stmt=$db->prepare($log_sql);
-    $log_stmt->bind_param('ii',$_SESSION['login_user'],$_SESSION['login_user']);
-    $log_stmt->execute();
-    $log_sql->close();
-    $output[3]=1;
+    $info_sql='select current_price(?,?)*?';
+    $info_stmt = $db->prepare($info_sql);
+    $info_stmt->bind_param('iii',$_SESSION['login_user'],$input['pid'],$input['qty']);
+    $info_stmt->execute();
+    $info_stmt->bind_result($revenue);
+    $info_stmt->fetch();
+    $output['revenue']=$revenue;
+    $output['revenue_after_tax']=(int)($revenue*0.95);
+
 
     print(json_encode($output));
 }
