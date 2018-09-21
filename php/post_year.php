99b2003248d871dfb4182e94717963b19a727373
Marty <MartyZou20010126@outlook.com>
2018-09-21 10:59:31 +0800

    New update

--- php/post_year.php
+++ php/post_year.php
@@ -12,6 +12,7 @@ else {
     $input = json_decode(file_get_contents("php://input"), true);
 
 
+
     $log_sql='insert into log (uid, yid, company_value) values (?,year_now(), (select (cash+20) from company_info where user_id=?))';
     $log_stmt=$db->prepare($log_sql);
     $log_stmt->bind_param('ii',$_SESSION['login_user'],$_SESSION['login_user']);
